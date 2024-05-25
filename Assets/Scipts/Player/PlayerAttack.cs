67010f80a5aeb8c64e5c3d889843beac24db5ec2
McChristie-Aidan <55122417+McChristie-Aidan@users.noreply.github.com>
2024-05-25 17:47:17 -0500

    added charge punch animation

--- Assets/Scipts/Player/PlayerAttack.cs
+++ Assets/Scipts/Player/PlayerAttack.cs
@@ -1,18 +1,56 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Interactions;
+using UnityEngine.UI;
 
 public class PlayerAttack : MonoBehaviour
 {
-    public GameObject textObject;
-    private TextMeshProUGUI debugText;
+
+    enum AttackState
+    {
+        Idle,
+        RegularAttack,
+        ChargeAttack,
+        GrabAttack
+    }
+
+    enum HandStante
+    {
+        Unarmed,
+        HoldingObject
+    }
+
     private Animator anim;
 
     bool rightOrLeft = true;
+
+    AttackState attackState = AttackState.Idle;
+
+    /*
+     * Charge attack
+     */
+    [SerializeField]
+    private float chargeAttackRate = .1f;
+    private float chargeAttackCurrentCharge = 0f;
+    private bool isCharging;
+
+    /*
+     * Rapid attack
+     */
+    [SerializeField]
+    private float rapidAttackWindow = .2f;
+    [SerializeField]
+    private int numOfClicks = 3;
+    private float rapidAttackCooldown;
+    private bool isRapidAttacking;
+
+    /*
+     * Debug 
+     */
+    public GameObject textObject;
+    private TextMeshProUGUI debugText;
+    public Image chargeWheel;
 
     // Start is called before the first frame update
     void Start()
@@ -21,48 +59,71 @@ public class PlayerAttack : MonoBehaviour
         InputAction action = new InputAction();
         debugText = textObject.GetComponent<TextMeshProUGUI>();
     }
+    private void Update()
+    {
+        if (isCharging && chargeAttackCurrentCharge < 1)
+        {
+            Debug.Log("Charge is Increasing!!");
+            chargeAttackCurrentCharge += chargeAttackRate*Time.deltaTime;
+            Debug.Log("Charge Amount: " + chargeAttackCurrentCharge);
+
+            anim.SetFloat("ChargeAmount", chargeAttackCurrentCharge);
+        }
+
+        if(chargeWheel != null)
+        {
+            chargeWheel.fillAmount = chargeAttackCurrentCharge;
+        }
+    }
     public void OnFire(InputAction.CallbackContext callbackContext)
     {
-        if (callbackContext.started && anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
-        {
-            if (debugText != null)
-            {
-                debugText.text = "Attack State: Regular Punch";
-            }
-            Debug.Log("punched!!");
-            if (rightOrLeft)
-            {
-                anim.Play("RightPunch");          
-            }
-            else
-            {
-                anim.Play("LeftPunch");
-            }
-            rightOrLeft = !rightOrLeft;
-        }
+        
         if (callbackContext.performed)
         {
             if (callbackContext.interaction is HoldInteraction)
             {
+                isCharging = true;
+                anim.SetBool("IsCharging", true);
+
                 if (debugText != null)
-                {
+                {                    
                     debugText.text = "Attack State: Charge Punch Started";
                 }
             }
-            else if (callbackContext.interaction is MultiTapInteraction)
+            else
             {
-                if (debugText != null)
+                if (callbackContext.interaction is TapInteraction && anim.GetCurrentAnimatorStateInfo(0).IsName("Idle")) 
                 {
-                    debugText.text = "Attack State: Ora Ora!!!";
+                    if (debugText != null)
+                    {
+                        debugText.text = "Attack State: Regular Punch";
+                    }
+                    Debug.Log("punched!!");
+                    if (rightOrLeft)
+                    {
+                        anim.Play("RightPunch");
+                    }
+                    else
+                    {
+                        anim.Play("LeftPunch");
+                    }
+                    rightOrLeft = !rightOrLeft;            
                 }
             }
         }
       
         if (callbackContext.canceled)
         {
-            if (debugText != null)
+            if (callbackContext.interaction is HoldInteraction)
             {
-                debugText.text = "Attack State: Charge Punch Cancelled";
+                chargeAttackCurrentCharge = 0f;
+                isCharging = false;
+                anim.SetBool("IsCharging", false);
+
+                if (debugText != null)
+                {
+                    debugText.text = "Attack State: Charge Punch Cancelled";
+                }         
             }
         }
     }
